9234e508a4b1b726081b15453f5a3365e7944f51
wukong919 <david_zhangw@outlook.com>
2024-09-04 07:54:05 +1000

    Update main.tf
    move the output to outputs.tf

--- main.tf
+++ main.tf
@@ -38,21 +38,3 @@ resource "vcf_credentials_rotate" "rotate" {
     ]
   }
 }
-
-
-output "secrets" {
-  value = {
-    for key, i in vcf_credentials_rotate.rotate : key => {
-      resource_name    = i.resource_name
-      resource_type    = i.resource_type
-      credential_type  = i.credentials[0].credential_type
-      user_name        = i.credentials[0].user_name
-      password         = i.credentials[0].password
-      secret_id        = i.id
-      last_rotate_time = i.last_rotate_time
-      domain           = local.credentials_map[key].resource_domain
-      sddc_manager     = var.sddc_manager_host
-    }
-  }
-  sensitive = true
-}
